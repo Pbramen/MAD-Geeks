f0bf599f21877a45031865c4f530010b5db3413f
Peter Banh <peterbanhe.e@gmail.com>
2024-03-08 19:06:52 -0600

    Added placeholder login functionality

--- client/mad-geeks/src/components/SignInForm.js
+++ client/mad-geeks/src/components/SignInForm.js
@@ -5,17 +5,38 @@ function SignInForm() {
     const nav = useNavigate();
     const [error, setError] = useState(null);
 
-    const validate = function (e) {
+    const validate = async function (e) {
         e.preventDefault();
         //console.log(e.target.username.value);
         //console.log(e.target.password.value);
-        //check for valid password, if true: 
-        if (true) {
-            nav('/home');
-        }
-        else { 
-            setError("Value here");
-        }
+        const headers = {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                "Content-Type": 'application/json',
+                "Origin": "http://localhost:3000",
+            },
+            body: JSON.stringify({ username: e.target.username.value, password: e.target.password.value })
+        };
+   
+        fetch("/api/clients/validateAuth", headers)
+            .then(async (response) => {
+                const json = await response.json();
+                return json;
+            })
+            .then((json) => {
+                if (json.status === 'ok') {
+                    nav('/home');
+                }
+                else {
+                    setError('Some error here....');
+                }
+            })
+            .catch((err) => { 
+                //TODO: log error here
+                console.log(err);
+            })
     }
     return (
         <section className="login-section">
