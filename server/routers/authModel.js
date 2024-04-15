26d5af1951c04a013ebca0b538088e656b94b37b
Peter Banh <peterbanhe.e@gmail.com>
2024-04-15 09:35:34 -0500

    Re-located auth files to their own directory

--- server/routers/authModel.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { test1, createNewUser, isValidAuth } = require('./authController.js');
-
-router.get('/', test1)
-router.post('/newUser', createNewUser)
-router.post('/validateAuth', isValidAuth);
-
-module.exports = router;
