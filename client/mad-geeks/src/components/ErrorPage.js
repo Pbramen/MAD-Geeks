f6c077bbec8dd82413ebf8cb5d42dc470269a242
Peter Banh <peterbanhe.e@gmail.com>
2024-05-10 16:46:13 -0500

    Fixed useRouteError()

--- client/mad-geeks/src/components/ErrorPage.js
+++ client/mad-geeks/src/components/ErrorPage.js
@@ -1,9 +1,13 @@
 import "../assets/css/error.css";
+import { useRouteError } from 'react-router-dom';
+
 export function ErrorPage() {
+    const err = useRouteError();
+    
     return (
         <div className="error">
             <h1 className="errorHeading">Sorry! Something went wrong.</h1>
-            
+            {err && <p>{ err.statusText || err.message }</p>}
           
         </div>
     );
