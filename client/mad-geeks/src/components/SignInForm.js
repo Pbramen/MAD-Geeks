5ca1e00365f91119e6be7b8010a727122aea6c18
Peter Banh <peterbanhe.e@gmail.com>
2024-05-10 16:50:49 -0500

    Fixed CORS bug, removed uncessary comments

--- client/mad-geeks/src/components/SignInForm.js
+++ client/mad-geeks/src/components/SignInForm.js
@@ -1,37 +1,37 @@
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import { setJWTCookie, decodeJWT } from "../assets/js/cookieHandler";
 
 function SignInForm() {
     const nav = useNavigate();
     const [error, setError] = useState(null);
-
+    
     const validate = async function (e) {
         e.preventDefault();
-        //console.log(e.target.username.value);
-        //console.log(e.target.password.value);
         const headers = {
-            method: 'GET',
+            method: 'POST',
             mode: 'cors',
             credentials: 'include',
             headers: {
                 "Content-Type": 'application/json',
                 "Origin": "http://localhost:3000",
+                "Accept": "application/json"
             },
             body: JSON.stringify({ username: e.target.username.value, password: e.target.password.value })
         };
    
-        fetch("/api/clients/validateAuth", headers)
+        fetch("http://localhost:4000/api/clients/validateAuth", headers)
             .then(async (response) => {
                 const json = await response.json();
                 return json;
             })
             .then((json) => {
                 // need some other message....
-                if (json.status === 'ok') {
-                    nav('/home');
+                if (json.status && json.status === 'SUCCESS' && json.msg && json.msg === "AUTH_OK" ) {
+                    nav(json.link );
                 }
                 else {
-                    setError('Some error here....');
+                    setError(json.msg);
                 }
             })
             .catch((err) => { 
@@ -39,18 +39,21 @@ function SignInForm() {
                 console.log(err);
             })
     }
+
+
     return (
         <section className="form-card">
             <div className="border"></div>
             <div className="card-banner">MadGeeks</div>
             <div className="form-group">
+                {error && <div className='alert'>*Invalid username or password.</div>}
                 <h2 className="sign-in-title">Sign In</h2>
                 <form className="sign-in-form" onSubmit={validate} method="GET">
                     <label htmlFor="username" className="form-label"><em>username/email:</em></label>
-                    <input type="text" id="username" name="username" required></input>
+                    <input type="text" id="username" name="username" className={error ? "invalid-field": ""} required></input>
 
                     <label htmlFor="password" className="form-label" required><em>password:</em></label>
-                    <input type="password" id="password" name="password"></input>
+                    <input type="password" id="password" name="password" className={error ? "invalid-field": ""}></input>
 
                     <button type="submit" className="login">
                         Login
@@ -59,7 +62,6 @@ function SignInForm() {
         
                 <a>Register for new account</a>
             </div>
-            {error && <p>{error}</p>}
         </section>
     )
 }
