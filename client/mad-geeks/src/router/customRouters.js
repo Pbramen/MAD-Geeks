e31bc53c71561c5da5f66b83f4548607bcc983a4
Peter Banh <peterbanhe.e@gmail.com>
2024-05-10 16:49:48 -0500

    added errorPage

--- client/mad-geeks/src/router/customRouters.js
+++ client/mad-geeks/src/router/customRouters.js
@@ -3,20 +3,37 @@ import { createBrowserRouter} from 'react-router-dom';
 import App from '../App';
 import { ErrorPage } from '../components/ErrorPage';
 import { DashBoard } from '../components/DashBoard';
+import SignInForm from '../components/SignInForm';
+import { CampaginPage } from '../components/CampaignPage';
+import { CharacterPage } from '../components/CharacterPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'home',
+        element: <DashBoard />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: 'login',
+        element: <SignInForm />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: 'campagins',
+        element: <CampaginPage />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: 'characters',
+        element: <CharacterPage />,
+        errorElement: <ErrorPage />
+      }
+      ] 
   }, 
-  {
-    path: '/home',
-    element: <DashBoard />
-  },
-  {
-    path: "*",
-    element: <ErrorPage />
-  }
 ])
 
