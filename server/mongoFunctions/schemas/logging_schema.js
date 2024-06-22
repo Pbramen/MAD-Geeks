ad16c23fba7cd7b4cba3357700976d94d45e3b47
Peter Banh <peterbanhe.e@gmail.com>
2024-06-22 18:13:12 -0500

    Placeholder logger schema.

---
+++ server/mongoFunctions/schemas/logging_schema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// records 100 - 499 status codes
+const api_access = new Schema({
+    protocol: String,
+    user_agent: String,
+    method: String,
+    endpoint: String,
+    status: Number,
+    req_headers: {
+        type: Object
+    },
+    user: {
+        _id: mongoose.SchemaTypes.ObjectId,
+        roles: [],
+    },
+    duration_ms: {
+        type: Number
+    },
+    timestamp: Date
+})
+
+const error = new Schema({
+    source: String,
+    status: Number,
+    type: String,
+    msg: String,
+    location: {
+        file: String,
+        line: Number,
+        column: Number
+    },
+    stack_trace: [String]
+})
+
+// Record system errors (usually returns 500+ status codes)
+const system_error = new Schema({
+    endpoint: String,
+    method: String,
+    payload: Object,
+    errors: [{
+        error
+    }],
+    action: String, 
+    user_agent: String,
+    version: String,
+})
