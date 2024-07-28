d7fc15add0ae5a65c020950c86298776cb4293b8
Peter Banh <peterbanhe.e@gmail.com>
2024-07-28 12:46:52 -0500

    removed unecessary tests

--- server/tests/mongoTests/errorHandler/mongoSetServer.stub.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const setUpForAPI = async (mongoDB) => {
-    
-}
-
-module.exports = {setUpForAPI}
---
+++ server/tests/noSQLPen/pythonProject4/injection.test.py
@@ -0,0 +1,9 @@
+# testing in python for potential automatic CI/CD
+import requests
+import json
+import pytest
+
+def syntax_test():
+    return None
+
+
