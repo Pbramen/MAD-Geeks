3c4cd9edd2fdb3762339286cae95bd24216f80bb
Peter Banh <peterbanhe.e@gmail.com>
2024-08-06 10:02:36 -0500

    included brief summary option for toggle tab components

--- client/mad-geeks/src/components/FieldSet.tsx
+++ client/mad-geeks/src/components/FieldSet.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ToggleTab } from 'components/ToggleTab';
 
-export const FieldSet = ({ children, legend_title, toggle=false }: { children: React.ReactNode, legend_title: string, toggle?: boolean }) => {
+export const FieldSet = ({ children, legend_title, description='', toggle=false }: { children: React.ReactNode, description?: string,  legend_title: string, toggle?: boolean }) => {
     return (
         <fieldset className="form-section">
             <legend className="form-section-title">{legend_title}</legend>
-            {toggle ? <ToggleTab>{children}</ToggleTab> : children}
+            {toggle ? <ToggleTab description={description}>{children}</ToggleTab> : children}
         </fieldset>
     )
 }
--- client/mad-geeks/src/components/ToggleTab.tsx
+++ client/mad-geeks/src/components/ToggleTab.tsx
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, MutableRefObject } from "react";
 import up from '../assets/svg/arrow-circle-up-svgrepo-com.svg';
 import down from '../assets/svg/arrow-circle-down-svgrepo-com.svg';
 
-export const ToggleTab = ({ children }: {children: React.ReactNode}) => {
-    const [display, setDisplay] = useState<boolean>(false);
-    const [img, setImage] = useState(up);
+export const ToggleTab = ({ children, description }: {children: React.ReactNode, description: string}) => {
+    const [display, setDisplay] = useState<boolean>(true);
+    const [img, setImage] = useState(down);
+    const expandableTab: React.MutableRefObject<any> = useRef();
 
     useEffect(() => {
-        if (display) {
-            setImage(up);
-        }
-        else {
-            setImage(down);
+        display ? setImage(()=>up) : setImage(()=>down);
+
+        if (expandableTab.current) {
+            const max_height = expandableTab.current.scrollHeight;
+            expandableTab.current.maxHeight = display ? `${max_height}px` : `50px`;
         }
     }, [display])
+
     const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setDisplay((prev) => !prev);
     }
 
     return (
-        <div className="flex flex-column toggle">
-            <div className="toggle-item">
-                <button style={{
-                    "background": "transparent",
-                    "border": 'none'
-                }} type="button" onClick={handleOnClick} className={`${display ? "toggle-on" : "toggle-off"}`}>
-                    <img width="20px" height="20px" src={img} alt={'Toggle Tab'} />
-                </button>
+        <div ref={expandableTab}  className={`flex flex-column toggle ${ display ? "expand" : "collapse"}`}>
+            <div className="flex flex-row" style={{
+                "justifyContent" : 'space-between'
+            }}>
+                <summary style={{ "fontWeight": "bold", "textAlign": "center", "flexGrow": "1", "paddingTop": "20px", "fontSize": "1.2em"}}>{description}</summary>
+                <div className="toggle-item">
+                    <button style={{
+                        "background": "transparent",
+                        "border": 'none'
+                    }} type="button" onClick={handleOnClick} className={`${display ? "toggle-on" : "toggle-off"}`}>
+                        <img width="30px" height="30px" src={img} alt={'Toggle Tab'} />
+                    </button>
+                </div>
             </div>
               {display && <div className="hidden-item-wrapper">{children} </div>}
         </div>
